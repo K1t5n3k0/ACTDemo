28df0c98165a837683f734aa23e01c673575070b
k1t5n3k0 <Gangc9525@gmail.com>
2022-08-31 00:23:33 +0800

    提交移动修改

--- Assets/Res/ActorController.cs
+++ Assets/Res/ActorController.cs
@@ -6,14 +6,19 @@ public class ActorController : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject model;
-    public PlayerInput pi;    
-    
+    public PlayerInput pi;
+
+    public float RunSpeed = 2.0f;
+    [SerializeField]
+    private Vector3 movingVec;
     private Animator anim;
+    private Rigidbody rigid;
 
     void Awake()
     {
         pi = GetComponent<PlayerInput>();
         anim = model.GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -23,5 +28,14 @@ public class ActorController : MonoBehaviour
         if(pi.Dmag > 0.1f){
             model.transform.forward = pi.Dvec;
         }
+
+        movingVec = pi.Dmag * model.transform.forward;
+    }
+
+    private void FixedUpdate()
+    {
+        //rigid.position += new Vector3( 0,0,1.0f) * Time.fixedDeltaTime;
+        //print(rigid.position);
+        rigid.position = rigid.position + RunSpeed * movingVec * Time.fixedDeltaTime;
     }
 }
