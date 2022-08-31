e5090b0aa063be678dcb2a38e602ae333b16398f
oshino <chengang@dpstorm.com>
2022-08-31 19:02:06 +0800

    提交0831开发

--- Assets/Res/ActorController.cs
+++ Assets/Res/ActorController.cs
@@ -8,11 +8,16 @@ public class ActorController : MonoBehaviour
     public GameObject model;
     public PlayerInput pi;
 
-    public float RunSpeed = 2.0f;
+    public float walkSpeed = 2.0f;
+    public float runSpeed = 3.0f;
     [SerializeField]
     private Vector3 movingVec;
     private Animator anim;
     private Rigidbody rigid;
+    private Vector3 planerVec;
+    private Vector3 thrustVec;
+
+    private bool lockPlaner = false;
 
     void Awake()
     {
@@ -24,18 +29,49 @@ public class ActorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        anim.SetFloat("forward", pi.Dmag);
+        //设置动画混合树
+        //使用lerp函数做插值，使切换不那么生硬
+        anim.SetFloat("forward", pi.Dmag * Mathf.Lerp(anim.GetFloat("forward"), ((pi.run) ? runSpeed : 1.0f), 0.1f));
+        //设跳跃信号
+        if (pi.jump)
+        {
+            anim.SetTrigger("jump");
+        }
+        
+        //设置人物朝向
         if(pi.Dmag > 0.1f){
-            model.transform.forward = pi.Dvec;
+            model.transform.forward = Vector3.Slerp (model.transform.forward, pi.Dvec, 0.2f);
         }
 
-        movingVec = pi.Dmag * model.transform.forward;
+        if(lockPlaner == false)
+        {
+            //planerVec = walkSpeed * pi.Dmag * model.transform.forward * ((pi.run) ? runSpeed : 1.0f);
+            planerVec = pi.Dmag * model.transform.forward * walkSpeed * ((pi.run) ? runSpeed : 1.0f);
+        }
+        //movingVec = walkSpeed * pi.Dmag * model.transform.forward * ((pi.run) ? runSpeed : 1.0f);
     }
 
     private void FixedUpdate()
     {
         //rigid.position += new Vector3( 0,0,1.0f) * Time.fixedDeltaTime;
         //print(rigid.position);
-        rigid.position = rigid.position + RunSpeed * movingVec * Time.fixedDeltaTime;
+        //rigid.position = rigid.position + planerVec * Time.fixedDeltaTime + thrustVec;
+        rigid.velocity = new Vector3(planerVec.x, rigid.velocity.y, planerVec.z) + thrustVec;
+        thrustVec = Vector3.zero;
+    }
+
+    public void JumpEnter()
+    {
+        pi.SWITCH = false;
+        lockPlaner = true;
+        thrustVec = new Vector3(0, 4f, 0);
+        //print("JumpEnter");
+    }
+
+    public void JumpExit()
+    {
+        pi.SWITCH = true;
+        lockPlaner = false;
+        //print("JumpExit");
     }
 }
--- Assets/Res/PlayerInput.cs
+++ Assets/Res/PlayerInput.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    public bool SWITCH = true;
-
+    [Header("===== Key settings=====")]
     public string KeyUp = "w";    
     public string KeyDown = "s";   
     public string KeyLeft = "a";
     public string KeyRight = "d";
 
+    public string KeyA = "left shift";
+    public string KeyB = "space";
+    public string KeyC;
+    public string KeyD;
+
+    [Header("=====Output signals=====")]
     public float Dup;
     public float Dright;
     public float Dmag;
     public Vector3 Dvec;
-    
+    //1.pressing signal
+    public bool run;
+    //2.trigger signal
+    public bool jump;
+    private bool lastJump;
+    //3.double sognal
+
+    [Header("=====Others=====")]
+    public bool SWITCH = true;   
+
     public float targetDup;
     public float targetDright;
     public float velocityDup;
@@ -31,9 +45,10 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //将玩家输入做一个转换
         targetDup = (Input.GetKey(KeyUp)?1.0f:0) - (Input.GetKey(KeyDown)?1.0f:0);
         targetDright = (Input.GetKey(KeyRight)?1.0f:0) - (Input.GetKey(KeyLeft)?1.0f:0);
-        
+        //开关
         if(SWITCH == false){
             targetDup = 0;
             targetDright = 0;
@@ -41,7 +56,35 @@ public class PlayerInput : MonoBehaviour
 
         Dup = Mathf.SmoothDamp (Dup, targetDup, ref velocityDup, 0.1f);
         Dright = Mathf.SmoothDamp (Dright, targetDright, ref velocityDright, 0.1f);
-        Dmag = Mathf.Sqrt((Dup*Dup) + (Dright * Dright));
-        Dvec = Dright * transform.right + Dup * transform.forward;
+        //解决斜向走路的时候会出现的加速问题
+        Vector2 temp = SquareToCricle(new Vector2(Dright, Dup));
+
+        float Dright2 = temp.x;
+        float Dup2 = temp.y;
+
+        Dmag = Mathf.Sqrt((Dup2*Dup2) + (Dright2 * Dright2));
+        Dvec = Dright2 * transform.right + Dup2 * transform.forward;
+
+        run = Input.GetKey(KeyA);
+        bool newjump = Input.GetKey(KeyB);
+        if (newjump != lastJump && newjump == true)
+        {
+            jump = true;
+
+        }
+        else
+        {
+            jump = false;
+        }
+        lastJump = newjump;
+    }
+
+    private Vector2 SquareToCricle(Vector2 input){
+        Vector2 output = Vector2.zero;
+
+        output.x = input.x * Mathf.Sqrt(1 - (input.y * input.y) / 2.0f);
+        output.y = input.y * Mathf.Sqrt(1 - (input.x * input.x) / 2.0f);
+
+        return output;
     }
 }
